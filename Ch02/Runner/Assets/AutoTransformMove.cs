3ffe136b3a8d342afe274c59aeb8ed6e34de4073
DaveLehman <lemo@provide.net>
2021-04-29 05:08:00 -0400

    Ch02.5 first platforms

---
+++ Ch02/Runner/Assets/AutoTransformMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GPC;
+
+public class AutoTransformMove : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector3 moveVector;
+    
+    private Transform _tr;  // this object's transform
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // keep move speed updated from game manager
+        moveSpeed = RunMan_GameManager.instance.runSpeed;
+
+        //move the transform
+        _tr.Translate((moveVector * moveSpeed) * Time.deltaTime);
+    }
+
+    public void SetMoveSpeed(float aSpeed)
+    {
+        // amethod to adjust speed from other scripts
+        moveSpeed = aSpeed;
+    }
+}
---
+++ Ch02/Runner/Assets/RunMan_GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GPC;
+
+public class RunMan_GameManager : BaseGameManager
+{
+    public float runSpeed = 1f;
+    public float distanceToSpawnPlatform = 2f;
+    public bool isRunning;
+    public static RunMan_GameManager instance { get; private set; }
+    // constructor
+    public RunMan_GameManager()
+    {
+        if(instance != null)
+        {
+            Debug.Log("RunMan_GameManager constructor has been called -- There is already a RunMan_GameManager instance running, so we're not making another one");
+        }
+        else
+        {
+            instance = this;
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
